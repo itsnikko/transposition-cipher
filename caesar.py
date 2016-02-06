e027e68db67bcbc4478efe3a334d50e07dff6d92
Nikko Williard <nikko.williard@gmail.com>
2016-02-06 16:58:42 -0500

    Updated the code. Menu work, logic...questionable

--- caesar.py
+++ caesar.py
@@ -10,59 +10,59 @@ alphabet = "abcdefghijklmnopqrstuvwxyz" + \
          ":.;,?!@#$%&()+=-*/_<> []{}`~^"
 
 """
-Prompts the user if they would like
-to encrypt or decrypt the text.
-"""
-vaild = false
-
-while not valid:
-
-
-    method = input('(E)ncryption or (D)ecryption? ')
-    if method[0].toLower() == "e":
-        prompt()
-        method = encryption()
-    elif method[0].toLower() == "d":
-        prompt()
-        method = decryption()
-    else:
-        print method
-
-"""
 All user Prompts
 """
-def prompt (method):
+def prompt ():
 
     """
     Prompts user for a message
     (Plaintext or ciphertext)
     """
-    message = input('Message: ')
+    message = raw_input('Message: ')
 
     """
     Prompts user for an
     interger key to encrypt with
     """
-    key = input(int, 'Key: ')
+    key = raw_input('Key: ')
 
     """
     Prompts user for a direction
     to encrypt (Left or Right)
     """
-    direction = input('(L)eft or (R)ight? ')
-    if direction[0].toLower() == "l":
+    direction = raw_input('(L)eft or (R)ight? ')
+    if direction[0].strip() == "l":
         direction = "-"
-    elif direction[0].toLower() == "r":
-        directon = "+"
+    elif direction[0].strip() == "r":
+        direction = "+"
     else:
         print direction
 
 """
 Encryption method
 """
-def encryption (message,key,alphabet):
-
+def encryption (message, key, alphabet):
+""" Need to convert the string to individual chars and add the key
+to the chars, then join back to a string. (i think)
+"""
+    return" ".join(str(ord(char)) for char in message)
 """
 Decryption method
 """
 def decryption (message,key,alphabet):
+    pass
+
+"""
+Prompts the user if they would like
+to encrypt or decrypt the text.
+"""
+while True:
+    method = raw_input('(E)ncryption or (D)ecryption? ')
+    if method[0].strip() == "e":
+        prompt()
+        encryption()
+    elif method[0].strip() == "d":
+        prompt()
+        decryption()
+    else:
+        pass
