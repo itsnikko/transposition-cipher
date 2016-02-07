97abce388ee50f16f474a39d5bd90e27063ed21f
Nikko Williard <nikko.williard@gmail.com>
2016-02-07 00:10:46 -0500

    Almost finished. Working on the rotation problem

--- caesar.py
+++ caesar.py
@@ -5,20 +5,21 @@ Professor Chaim Sanders a.k.a Col. Sanders
 Nikko Williard and Sonjay Sivarajah
 February 6, 2016
 """
-alphabet = "abcdefghijklmnopqrstuvwxyz" + \
-     "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"+ \
-         ":.;,?!@#$%&()+=-*/_<> []{}`~^"
 
 """
 All user Prompts
 """
-def prompt ():
+message = ''
+key = ''
 
+def prompt ():
+    global message
+    global key
     """
     Prompts user for a message
     (Plaintext or ciphertext)
     """
-    message = raw_input('Message: ')
+    message = raw_input('Message: ').lower()
 
     """
     Prompts user for an
@@ -26,32 +27,36 @@ def prompt ():
     """
     key = raw_input('Key: ')
 
-    """
-    Prompts user for a direction
-    to encrypt (Left or Right)
-    """
-    direction = raw_input('(L)eft or (R)ight? ')
-    if direction[0].strip() == "l":
-        direction = "-"
-    elif direction[0].strip() == "r":
-        direction = "+"
-    else:
-        print direction
-
 """
 Encryption method
 """
-def encryption (message, key, alphabet):
-""" Need to convert the string to individual chars and add the key
-to the chars, then join back to a string. (i think)
-"""
-    return" ".join(str(ord(char)) for char in message)
+def encryption (message, key):
+    """
+    Need to convert the string to individual chars and add the key
+    to the chars, then join back to a string. (i think)
+    """
+    ret = ""
+    for char in message:
+        """
+        rotation testing
+        """
+        ret += chr((ord(char) - ord('a') + int(key) % 26) + ord('a'))
+    print ret
+    exit()
 """
 Decryption method
 """
-def decryption (message,key,alphabet):
-    pass
-
+def decryption (message,key):
+    ret = ""
+    for char in message:
+        var = (ord(char) - int(key)) % 26
+        if var < 33:
+            var += 33
+        if var < 0:
+            var += 126
+        ret += chr(var)
+    print ret
+    exit()
 """
 Prompts the user if they would like
 to encrypt or decrypt the text.
@@ -60,9 +65,9 @@ while True:
     method = raw_input('(E)ncryption or (D)ecryption? ')
     if method[0].strip() == "e":
         prompt()
-        encryption()
+        encryption(message,key)
     elif method[0].strip() == "d":
         prompt()
-        decryption()
+        decryption(message,key)
     else:
         pass
